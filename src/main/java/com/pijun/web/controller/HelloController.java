90334ad5944cf52bea7563ee362d5b193757f55e
zhoulijun <zhoulijun89@gmail.com>
2017-08-07 15:40:31 +0800

    freemarker 支持

--- src/main/java/com/pijun/web/controller/HelloController.java
+++ src/main/java/com/pijun/web/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.pijun.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +14,12 @@ public class HelloController {
     @RequestMapping("/world")
     @ResponseBody
     public String HelloWorld(){
-        return "World!";
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","这是一个ftl模板~");
+        return "hello";
     }
 }
---
+++ src/main/resources/webapp/view/hello.ftl
@@ -0,0 +1,12 @@
+<#assign basePath=request.contextPath>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <script src="${basePath}/js/test.js?"></script>
+</head>
+<body>
+${msg};
+</body>
+
+</html>
